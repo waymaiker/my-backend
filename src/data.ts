fdb5eb635afb2cf215e992213621f45185289e60
Bonkomprann <waymaiker@laposte.net>
2023-08-17 18:59:47 +0200

    Change data format

--- src/data.ts
+++ src/data.ts
@@ -1,11 +1,11 @@
 import { ChapterType } from "./dtos/chapter.dto";
-import { AccountType } from "./dtos/user.dto";
+import { ProfileType, Language } from "./dtos/user.dto";
 
 export interface Data {
   chapters: {
-    id: string,
+    id: number,
     name: string,
-    level: number,
+    max_level: number,
     created_at: Date,
     updated_at: Date,
     type: ChapterType
@@ -13,68 +13,83 @@ export interface Data {
   users: {
     id: string,
     pseudo: string,
-    level: number,
+    profile_language: Language,
+    finished_level: number,
     created_at: Date,
     updated_at: Date,
-    accountType: AccountType
+    profile_type: ProfileType
   }[]
 }
 
 export const data:Data = {
   chapters: [{
-    id: '0',
+    id: 0,
     name: 'introduction',
-    level: 5,
+    max_level: 4,
+    type: ChapterType.FREEMIUM,
     created_at: new Date(),
     updated_at: new Date(),
-    type: ChapterType.FREEMIUM
+  },
+  {
+    id: 1,
+    name: "greetings",
+    max_level: 5,
+    type: ChapterType.FREEMIUM,
+    created_at: new Date(),
+    updated_at: new Date(),
   }],
   users: [{
     id: "834e0b68-6926-4329-b346-3e6e61613b94",
     pseudo: 'PREMIUM',
-    level: 1,
+    profile_language: Language.ENGLISH,
+    finished_level: 1,
     created_at: new Date(),
     updated_at: new Date(),
-    accountType: AccountType.PREMIUM
+    profile_type: ProfileType.PREMIUM
   },
   {
     id: "575e7b94-ebf4-4e30-9478-969c85e9baf9",
     pseudo: 'PREMIUM',
-    level: 4,
+    profile_language: Language.ENGLISH,
+    finished_level: 4,
     created_at: new Date(),
     updated_at: new Date(),
-    accountType: AccountType.PREMIUM
+    profile_type: ProfileType.PREMIUM
   },
   {
     id: "4c609110-0c6d-4217-aad9-792d506b686f",
     pseudo: 'FREEMIUM',
-    level: 54,
+    profile_language: Language.ENGLISH,
+    finished_level: 54,
     created_at: new Date(),
     updated_at: new Date(),
-    accountType: AccountType.FREEMIUM
+    profile_type: ProfileType.FREEMIUM
   },
   {
     id: "2477c3be-f9fe-4b2d-8784-8fa5cae3b8ec",
     pseudo: 'FREEMIUM',
-    level: 3,
+    profile_language: Language.FRENCH,
+    finished_level: 3,
     created_at: new Date(),
     updated_at: new Date(),
-    accountType: AccountType.FREEMIUM
+    profile_type: ProfileType.FREEMIUM
   },
   {
     id: "97392b1f-123e-46f6-9e4c-b649e434484c",
     pseudo: 'FREEMIUM',
-    level: 23,
+    profile_language: Language.FRENCH,
+    finished_level: 23,
     created_at: new Date(),
     updated_at: new Date(),
-    accountType: AccountType.FREEMIUM
+    profile_type: ProfileType.FREEMIUM
   },
   {
     id: "b2ce1bba-d53b-4a09-82e8-c3bbb096c6f8",
     pseudo: 'FREEMIUM',
-    level: 1,
+    profile_language: Language.WOLOF,
+    finished_level: 1,
     created_at: new Date(),
     updated_at: new Date(),
-    accountType: AccountType.FREEMIUM
+    profile_type: ProfileType.FREEMIUM
   }]
 }
