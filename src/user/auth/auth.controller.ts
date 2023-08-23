5e93cacd8ea0a57902604e240ed0da01307e9e2c
Bonkomprann <waymaiker@laposte.net>
2023-08-24 00:35:27 +0200

    Sign in by userType

--- src/user/auth/auth.controller.ts
+++ src/user/auth/auth.controller.ts
@@ -1,5 +1,8 @@
-import { Body, Controller, Post } from '@nestjs/common';
-import { SignInDto, SignUpDto } from '../dtos/auth.dto';
+import * as bcrypt from 'bcryptjs';
+import { Body, Controller, Param, ParseEnumPipe, Post, UnauthorizedException } from '@nestjs/common';
+import { UserType } from '@prisma/client';
+
+import { GenerateProductKey, SignInDto, SignUpDto } from '../dtos/auth.dto';
 import { AuthService } from './auth.service';
 
 @Controller('auth')
@@ -7,13 +10,37 @@ export class AuthController {
 
   constructor(private readonly authService: AuthService){}
 
-  @Post("/signup")
-  signup(@Body() body: SignUpDto){
-    return this.authService.signup(body);
+  @Post("/signup/:userType")
+  async signup(
+    @Body() body: SignUpDto,
+    @Param('userType', new ParseEnumPipe(UserType)) userType: UserType,
+  ){
+    if(userType !== UserType.USER){
+      if(!body.productKey){
+        throw new UnauthorizedException()
+      }
+
+      const validProductKey = `${body.email}-${userType}-${process.env.PRODUCT_KEY_SECRET}`;
+      const isValidProductKey = await bcrypt.compare(validProductKey, body.productKey);
+
+      console.log(isValidProductKey);
+
+      if(!isValidProductKey) {
+        throw new UnauthorizedException()
+      }
+    }
+
+
+    return this.authService.signup(body, userType);
   }
 
   @Post("/signin")
   signin(@Body() body: SignInDto){
     return this.authService.signin(body);
+  }
+
+  @Post("/key")
+  generateProductKey(@Body() { userType, email }: GenerateProductKey){
+    return this.authService.generateProductKey(email, userType);
   }
 }
