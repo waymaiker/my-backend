f89b64dd62c923c8ce734dced7124a8dcde538ae
Bonkomprann <waymaiker@laposte.net>
2023-08-23 20:39:44 +0200

    Config prisma, update schema and use it

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -3,8 +3,10 @@ import { UserController } from './user.controller';
 import { UserService } from './user.service';
 import { AuthController } from './auth/auth.controller';
 import { AuthService } from './auth/auth.service';
+import { PrismaModule } from 'src/prisma/prisma.module';
 
 @Module({
+  imports: [PrismaModule],
   controllers: [UserController, AuthController],
   providers: [UserService, AuthService]
 })
