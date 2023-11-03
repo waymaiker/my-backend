be189cf4119aed39662997fb51fe77a10c4de010
Bonkomprann <waymaiker@laposte.net>
2023-11-03 23:39:59 +0100

    Enable cors

--- src/main.ts
+++ src/main.ts
@@ -4,13 +4,21 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe({
-    whitelist: true,
-    transform: true,
-    transformOptions: {
-      enableImplicitConversion: true
+  app.enableCors({
+    origin: '*',
+    methods: 'GET, PUT, POST, DELETE',
+    allowedHeaders: 'Content-Type, Authorization',
+  });
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+      transformOptions: {
+        enableImplicitConversion: true
+      }
     }
-  }))
+  ))
 
   await app.listen(3000);
 }
