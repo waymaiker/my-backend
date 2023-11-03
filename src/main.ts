db4f7760e4d02538add4fa2c031df5b236b7ab2e
Bonkomprann <waymaiker@laposte.net>
2023-11-04 00:08:43 +0100

    Fix listening port

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+const port = process.env.PORT || 3000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -21,6 +22,6 @@ async function bootstrap() {
     }
   ))
 
-  await app.listen(0o0);
+  await app.listen(port, "0.0.0.0");
 }
 bootstrap();
