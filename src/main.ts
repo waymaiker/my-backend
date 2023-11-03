6994f5bac88013e51bbf4df6875789a92a96530a
Bonkomprann <waymaiker@laposte.net>
2023-11-03 23:53:04 +0100

    Fix cors

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     origin: '*',
+    credentials: true,
     methods: 'GET, PUT, POST, DELETE',
     allowedHeaders: 'Content-Type, Authorization',
   });
